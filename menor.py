c66daa87f38efee931633ae57528b660a065d71b
bartur-code <b.artur@escolar.ifrn.edu.br>
2025-11-28 14:33:02 +0000

    fiz codigo exercicio funcao menor

---
+++ menor.py
@@ -0,0 +1,19 @@
+def maioroumenor(a, b, c, d, e):
+    menor = a 
+    if b < menor:
+        menor = b
+    if c < menor:
+        menor = c
+    if d < menor:
+        menor = d
+    if d < menor:
+        menor = e
+ 
+
+# 5 6 1 9 10   
+a = 5
+b = 6
+c = 1
+d = 9
+e = 10 
+maioroumenor = print(menor)
