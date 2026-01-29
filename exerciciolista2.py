cd9f7c8fa9f3de6a4850b6bbccefac0430c40c08
bartur-code <b.artur@escolar.ifrn.edu.br>
2026-01-29 15:02:12 +0000

    lol2

--- exerciciolista2.py
+++ exerciciolista2.py
@@ -2,3 +2,4 @@ lista = ["a", "oi", 26, 127000, 2, "batarde"]
 busca = input()
 for i in range(len(lista)):
  print(lista[i])
+ #verificar se o valor da lista é == a busca (if lista[i] == busca) return true, (else return "não tem")
